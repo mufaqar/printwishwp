a024626c19f343bd058ff5c7da40f419bb66c7db
mufaqar <mufaqar@gmail.com>
2024-04-24 13:54:42 +0500

    updated

--- functions.php
+++ functions.php
@@ -201,3 +201,9 @@ require get_template_directory() . '/inc/walker_nav.php';
 
 
 
+add_action( 'after_setup_theme', 'setup_woocommerce_support' );
+
+ function setup_woocommerce_support()
+{
+  add_theme_support('woocommerce');
+}
---
+++ woocommerce/single-product.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The Template for displaying all single products
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header( 'shop' ); ?>
+
+<?php
+		/**
+		 * woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		//do_action( 'woocommerce_before_main_content' );
+	?>
+<!-- <div class='block md:hidden px-3 container mx-auto'>
+    <div class='mt-3 flex justify-center'>
+        <Image src="<?php bloginfo('template_directory'); ?>/public/images/review-badge.svg" alt="rating" width={200}
+            height={45} />
+    </div>
+    <h2 class='text-xl md:text-3xl lg:text-4xl font-medium mt-6 md:mt-0'>title</h2>
+    <p class='mt-4 font-normal text-accent'>Product Code: <span class=''>sku</span></p>
+</div> -->
+
+<?php while ( have_posts() ) : ?>
+
+<?php the_post(); ?>
+
+<?php wc_get_template_part( 'content', 'single-product' ); ?>
+
+<?php endwhile; // end of the loop. ?>
+
+<?php
+		/**
+		 * woocommerce_after_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action( 'woocommerce_after_main_content' );
+	?>
+
+<?php
+		/**
+		 * woocommerce_sidebar hook.
+		 *
+		 * @hooked woocommerce_get_sidebar - 10
+		 */
+		do_action( 'woocommerce_sidebar' );
+	?>
+
+<?php
+get_footer( 'shop' );
+
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
