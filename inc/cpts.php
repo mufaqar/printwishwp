e4e1541a1182da8759ca28e968c9d3a1cb7a90e4
mufaqar <mufaqar@gmail.com>
2024-04-24 14:59:25 +0500

    cpts

--- inc/cpts.php
+++ inc/cpts.php
@@ -1,9 +1,5 @@
 <?php
 
-
-
-
-
 function cptui_register_my_cpts() {
 
 /**
@@ -126,3 +122,49 @@ register_post_type( "bags", $args );
 }
 
 add_action( 'init', 'cptui_register_my_cpts' );
+
+
+
+
+function cptui_register_my_cpts_reviews() {
+
+	/**
+	 * Post Type: Reviews.
+	 */
+
+	$labels = [
+		"name" => esc_html__( "Reviews", "printwish" ),
+		"singular_name" => esc_html__( "Review", "printwish" ),
+	];
+
+	$args = [
+		"label" => esc_html__( "Reviews", "printwish" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"can_export" => false,
+		"rewrite" => [ "slug" => "reviews", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "reviews", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_reviews' );
+
