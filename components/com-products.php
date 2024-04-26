b1c6dd9ee4afdfd7589f90d42e31503b14e14752
mufaqar <mufaqar@gmail.com>
2024-04-26 14:45:41 +0500

    products

--- components/com-products.php
+++ components/com-products.php
@@ -1,10 +1,8 @@
+<section><div class="py-6 relative md:py-16 bg-cover bg-no-repeat bg-center" style="background-image:linear-gradient(0deg, rgba(0, 36,63,0.7), rgba(0, 36,63,0.7)), url(/images/merge.png)"><div class="container mx-auto px-4  text-white"><h2 class="sm:text-4xl text-4xl font-bold font-opensans capitalize mb-2  sm:text-left text-center">Products</h2><div></div></div></div></section>
+
 <section class='py-16 relative'>
     <div class='max-w-screen-xl mx-auto px-4 '>
-        <div class='w-fit mx-auto mb-10'>
-            <h2 class='sm:text-4xl text-2xl font-semibold font-opensans text-accent uppercase text-center mb-5'>
-               PRODUCTS
-            </h2>
-        </div>
+        
         <div class='w-full'>
 
             <div class='grid sm:grid-cols-2 md:grid-cols-5  grid-cols-2 gap-1 sm:gap-2 md:gap-4'>
