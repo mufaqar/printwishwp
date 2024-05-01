aac6a91aa5ecad75fe6fd1d0492e0e35d39d98ec
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2024-05-01 19:40:18 +0500

    Header Finall

--- components/header-nav.php
+++ components/header-nav.php
@@ -68,13 +68,6 @@ $Categories = array(
                                 d="M6 18L18 6M6 6l12 12"></path>
                         </svg>
                     </button>
-
-                    <!-- <button id="menu-toggle" class="cursor-pointer">
-                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" aria-hidden="true"
-                            height="24" width="24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h8m-8 6h16"></path>
-                        </svg>
-                    </button> -->
                     <a href="tel:08000510821">
                         <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024"
                             height="24" width="24" xmlns="http://www.w3.org/2000/svg">
@@ -99,7 +92,7 @@ $Categories = array(
                         </svg>
                     </button>
                     <div id="dropdown"
-                        class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow absolute top-[108px] ">
+                        class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow absolute -top-28">
                         <ul class="py-1 text-sm text-gray-700" aria-labelledby="dropdown-button">
                             <?php
                             if ($Categories && is_array($Categories)) {
@@ -110,7 +103,6 @@ $Categories = array(
                                             <?php echo $item['name']; ?>
                                         </a>
                                     </li>
-
                                     <?php
                                 }
                             }
@@ -155,7 +147,7 @@ $Categories = array(
                         </a>
                     </li>
                     <li class="relative">
-                        <button
+                        <button id="submenu-Categories"
                             class="flex justify-between items-center w-full font-medium md:p-0 md:w-auto text-white hover:text-primary">
                             Categories
                             <svg class="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
@@ -165,6 +157,34 @@ $Categories = array(
                                     clip-rule="evenodd"></path>
                             </svg>
                         </button>
+                        <div id="submenu-Cat" class="md:absolute md:mr-5 z-50 bg-secondary w-44 pt-4 md:pt-7 hidden">
+                            <ul class='md:pb-2'>
+                                <?php
+                                if ($Categories && is_array($Categories)) {
+                                    foreach ($Categories as $idx => $item) {
+                                        ?>
+                                        <li class="px-5 py-1.5 relative group">
+                                            <a href="<?php echo $item['link']; ?>"
+                                                class="text-xs cursor-pointer w-full group-hover:text-gray-700 text-white font-semibold uppercase">
+                                                <?php echo $item['name']; ?>
+                                            </a>
+                                            <span
+                                                class="absolute h-full p-[2px] bg-gray-700 top-0 left-0 hidden group-hover:block"></span>
+                                        </li>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                                <li class="px-5 py-1.5 relative group">
+                                    <a href="#"
+                                        class="text-xs cursor-pointer w-full group-hover:text-gray-700 text-white font-semibold uppercase">
+                                        All locations
+                                    </a>
+                                    <span
+                                        class="absolute h-full p-[2px] bg-gray-700 top-0 left-0 hidden group-hover:block"></span>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                     <li>
                         <a class="text-white hover:text-primary" href="<?php echo home_url('/about-us'); ?>">
@@ -183,7 +203,7 @@ $Categories = array(
                         </a>
                     </li>
                     <li class="relative">
-                        <button
+                        <button id="submenu-locations"
                             class="flex justify-between items-center w-full font-medium md:p-0 md:w-auto text-white hover:text-primary">
                             Locations
                             <svg class="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
@@ -193,175 +213,39 @@ $Categories = array(
                                     clip-rule="evenodd"></path>
                             </svg>
                         </button>
-                        <ul class="md:absolute md:mr-5 z-50 !bg-secondary !w-44 pt-4 md:pt-7 hidden submenu">
-                            <?php
-                            if ($Categories && is_array($Categories)) {
-                                foreach ($Categories as $idx => $item) {
-                                    ?>
-                                    <li>
-                                        <a href="<?php echo $item['link']; ?>" class="flex p-3 rounded-lg hover:bg-gray-50 ">
-                                            <?php echo $item['name']; ?>
-                                        </a>
-                                    </li>
-
-                                    <?php
-                                }
-                            }
-                            ?>
-
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- <nav class="bg-secondary z-50">
-        <div class="grid py-4 px-4 mx-auto max-w-screen-xl lg:grid-cols-2 md:px-6">
-            <form class="flex mb-4 lg:order-2 lg:mb-0">
-                <label for="search-dropdown"
-                    class="mb-2 text-sm font-medium text-gray-900 sr-only ">Your Email</label>
-                <button id="dropdown-button" data-dropdown-toggle="dropdown"
-                    class="hidden md:inline-flex flex-shrink-0 z-10 items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-200 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100"
-                    type="button">All categories <svg class="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
-                    </svg></button>
-                <div id="dropdown"
-                    class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow"
-                    data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top"
-                    style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(897px, 5637px, 0px);">
-                    <ul class="py-1 text-sm text-gray-700 " aria-labelledby="dropdown-button">
-                        <li>
-                            <button type="button"
-                                class="inline-flex py-2 px-4 w-full hover:bg-gray-100">Mockups</button>
-                        </li>
-                        <li>
-                            <button type="button"
-                                class="inline-flex py-2 px-4 w-full hover:bg-gray-100">Templates</button>
-                        </li>
-                        <li>
-                            <button type="button"
-                                class="inline-flex py-2 px-4 w-full hover:bg-gray-100">Design</button>
-                        </li>
-                        <li>
-                            <button type="button"
-                                class="inline-flex py-2 px-4 w-full hover:bg-gray-100">Logos</button>
-                        </li>
-                    </ul>
-                </div>
-                <div class="relative w-full">
-                    <input type="search" id="search-dropdown"
-                        class="block p-2.5 w-full text-sm text-accent bg-gray-50 rounded-lg md:rounded-l-none md:border-l-gray-50 border-l-1 md:border-l-6 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 outline-none"
-                        placeholder="Search anything..." required>
-                    <button type="submit"
-                        class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-primary rounded-r-lg border border-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary-300"><svg
-                            class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg></button>
-                </div>
-            </form>
-            <div class="flex items-center lg:order-1">
-
-                <?php
-                // wp_nav_menu(array(
-                //         'theme_location' => 'main',
-                //         'menu_class'     => 'nav-list', // Add a custom class to the menu
-                //         'walker'         => new Tailwind_Nav_Walker(),
-                //     ));
-                
-
-                ?>
-
-
-                <ul id="menu" class="flex flex-row mt-0 space-x-8 text-sm font-medium">
-                    <li>
-                        <a href="<?php echo home_url(''); ?>"
-                            class="text-white hover:text-primary"
-                            aria-current="page">Home</a>
-                    </li>
-                    <li>
-                        <button id="dropdown-button-megamenu" data-collapse-toggle="megamenu"
-                            class="flex justify-between items-center w-full font-medium md:p-0 md:w-auto text-white hover:text-primary ">Categories
-                            <svg class="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg></button>
-                    </li>
-                    <li>
-                        <a href="<?php echo home_url('/about-us'); ?>"
-                            class="text-white hover:text-primary">About
-                            Us</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo home_url('/product'); ?>"
-                            class="hidden text-white  hover:text-primary md:inline">Products</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo home_url('/contact-us'); ?>"
-                            class="hidden text-white  hover:text-primary md:inline">Contact</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo home_url('/locations'); ?>"
-                            class="hidden text-white  hover:text-primary md:inline">Locations</a>
-
-
-                            <ul class="col-span-2 md:col-span-1 absolute top-[10rem] bg-white hidden submenu">
-                                    <?php
-                                    if ($Categories && is_array($Categories)) {
-                                        foreach ($Categories as $idx => $item) {
-                                            ?>
-                                    <li>
-                                        <a href="<?php echo $item['link']; ?>"
-                                            class="flex p-3 rounded-lg hover:bg-gray-50 ">
-                                            <?php echo $item['name']; ?>
-                                        </a>
-                                    </li>
-
-                                    <?php
-                                        }
+                        <div id="submenu-loc" class="md:absolute md:mr-5 z-50 bg-secondary w-44 pt-4 md:pt-7 hidden">
+                            <ul class='md:pb-2'>
+                                <?php
+                                if ($Categories && is_array($Categories)) {
+                                    foreach ($Categories as $idx => $item) {
+                                        ?>
+                                        <li class="px-5 py-1.5 relative group">
+                                            <a href="<?php echo $item['link']; ?>"
+                                                class="text-xs cursor-pointer w-full group-hover:text-gray-700 text-white font-semibold uppercase">
+                                                <?php echo $item['name']; ?>
+                                            </a>
+                                            <span
+                                                class="absolute h-full p-[2px] bg-gray-700 top-0 left-0 hidden group-hover:block"></span>
+                                        </li>
+                                        <?php
                                     }
-                                    ?>
-
-                                    </ul> 
+                                }
+                                ?>
+                                <li class="px-5 py-1.5 relative group">
+                                    <a href="#"
+                                        class="text-xs cursor-pointer w-full group-hover:text-gray-700 text-white font-semibold uppercase">
+                                        All locations
+                                    </a>
+                                    <span
+                                        class="absolute h-full p-[2px] bg-gray-700 top-0 left-0 hidden group-hover:block"></span>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
-
                 </ul>
             </div>
         </div>
     </nav>
-    <nav id="megamenu" class="bg-white border-b border-gray-200 hidden">
-        <div
-            class="grid py-4 px-4 mx-auto max-w-screen-xl text-gray-900  md:grid-cols-2 lg:grid-cols-4 md:px-6">
-            <ul class="col-span-2 md:col-span-1">
-
-                <?php
-                if ($Categories && is_array($Categories)) {
-                    foreach ($Categories as $idx => $item) {
-                        ?>
-                <li>
-                    <a href="<?php echo $item['link']; ?>"
-                        class="flex p-3 rounded-lg hover:bg-gray-50">
-                        <?php echo $item['name']; ?>
-                    </a>
-                </li>
-
-                <?php
-                    }
-                }
-                ?>
-
-            </ul>
-
-        </div>
-    </nav> -->
-
-
 </header>
 
 <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
@@ -378,6 +262,25 @@ $Categories = array(
             menuIconOpen.classList.toggle('hidden');
             // Toggle the visibility of the menu
             menu.classList.toggle('hidden');
+        });
+    });
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const submenuLocations = document.getElementById('submenu-locations');
+        const submenuCategories = document.getElementById('submenu-Categories');
+        const submenuloc = document.getElementById('submenu-loc');
+        const submenuCat = document.getElementById('submenu-Cat');
+
+        submenuLocations.addEventListener('mouseenter', function () {
+            // Toggle the visibility of the submenu
+            submenuloc.classList.toggle('hidden');
+            submenuCat.classList.add('hidden');
+        });
+        submenuCategories.addEventListener('mouseenter', function () {
+            // Toggle the visibility of the submenu
+            submenuCat.classList.toggle('hidden');
+            submenuloc.classList.add('hidden');
         });
     });
 
