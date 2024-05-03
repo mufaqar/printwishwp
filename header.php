653399dca98d4eec182e82db9ff0deeb4baf1014
mufaqar <mufaqar@gmail.com>
2024-05-03 20:05:10 +0500

    index no follow

--- header.php
+++ header.php
@@ -14,12 +14,13 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+<meta name="robots" content="noindex, nofollow" />
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <a rel="profile" href="https://gmpg.org/xfn/11">
 
         <?php wp_head(); ?>
-        <meta name="robots" content="noindex">
+      
 </head>
 
 <body <?php body_class(); ?>>
