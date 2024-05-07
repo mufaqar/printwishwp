fa9b3339eb35764b006b5e57db8486e00b51d4f9
mufaqar <mufaqar@gmail.com>
2024-05-07 19:46:13 +0500

    updated

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -21,8 +21,17 @@ get_header('shop');
 
 $cat = get_queried_object();
 $term_id = get_queried_object_id();
+$term = get_queried_object();
+$term_faqs = get_field('faqs', $term);
 
-$term_meta = get_term_meta($term_id, 'faqs', true);
+// foreach ($term_faqs as $faq) {
+// 	print_r($faq);
+// }
+
+
+$term_content= get_term_meta($term_id, 'content', true);
+
+
 
 // Get the thumbnail URL for the term
 $thumbnail_id = get_term_meta($term_id, 'thumbnail_id', true);
@@ -37,18 +46,18 @@ if (!empty($thumbnail_id)) {
 ?>
 
 <section>
-	<div class="py-6 relative md:py-16 bg-cover bg-no-repeat bg-center"
-		style="background-image: linear-gradient(0deg, rgba(0, 36, 63, 0.7), rgba(0, 36, 63, 0.7)), url(<?php echo $cat_thumbnail ?>);">
-		<div class="container mx-auto px-4 text-white">
-			<h2 class="sm:text-4xl text-4xl font-bold font-opensans capitalize mb-2 sm:text-left text-center">
-				<?php echo esc_html($cat->name); ?>
-			</h2>
-			<div>
-				<?php echo wp_kses_post($cat->description); ?>
-			</div>
-		</div>
+    <div class="py-6 relative md:py-16 bg-cover bg-no-repeat bg-center"
+        style="background-image: linear-gradient(0deg, rgba(0, 36, 63, 0.7), rgba(0, 36, 63, 0.7)), url(<?php echo $cat_thumbnail ?>);">
+        <div class="container mx-auto px-4 text-white">
+            <h2 class="sm:text-4xl text-4xl font-bold font-opensans capitalize mb-2 sm:text-left text-center">
+                <?php echo esc_html($cat->name); ?>
+            </h2>
+            <div>
+                <?php echo wp_kses_post($cat->description); ?>
+            </div>
+        </div>
 
-	</div>
+    </div>
 </section>
 
 
@@ -62,7 +71,7 @@ get_template_part('components/com', 'reviews');
 ?>
 
 <div class="md:w-2/6 mx-auto">
-	<?php
+    <?php
 	// Usage
 	$title = 'Delivery Title';
 	$desc = 'Delivery Description';
@@ -71,12 +80,12 @@ get_template_part('components/com', 'reviews');
 </div>
 
 <section class='py-16 relative'>
-	<div class='max-w-screen-xl mx-auto px-4 '>
+    <div class='max-w-screen-xl mx-auto px-4 '>
 
-		<div class='w-full'>
-			<div class='grid sm:grid-cols-2 md:grid-cols-5  grid-cols-2 gap-1 sm:gap-2 md:gap-4'>
+        <div class='w-full'>
+            <div class='grid sm:grid-cols-2 md:grid-cols-5  grid-cols-2 gap-1 sm:gap-2 md:gap-4'>
 
-				<?php
+                <?php
 
 				if (woocommerce_product_loop()) {
 					//woocommerce_product_loop_start();
@@ -117,10 +126,84 @@ get_template_part('components/com', 'reviews');
 
 				?>
 
-			</div>
-		</div>
-	</div>
-</section><?php
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container mx-auto px-1 my-2 w-full">
+        <div className='category_pages mt-10'>
+            <?php echo $term_content ?>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container mx-auto px-1 my-2 w-full">
+
+        <?php
+
+
+
+
+
+	?>
+
+        <section class="bg-white mb-10">
+            <div class="">
+                <h2 class="mb-6 lg:mb-8 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900">
+                    Frequently asked questions</h2>
+                <div class="mx-auto ">
+                    <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white text-gray-900"
+                        data-inactive-classes="text-gray-500" class="flex flex-col gap-2">
+
+                        <?php foreach ($term_faqs as $key => $faq) { 
+						
+						$question = $faq['question'];
+						$answer = $faq['answer'];
+						?>
+
+                        <div class="bg-white p-3 px-5 border border-gray-200 font-opensans cursor-pointer">
+                            <h2 id="accordion-flush-heading-<?php echo $key; ?>">
+                                <button type="button"
+                                    class="flex justify-between items-center w-full font-medium hover:text-secondary"
+                                    data-accordion-target="#accordion-flush-body-<?php echo $key; ?>"
+                                    aria-expanded="true" aria-controls="accordion-flush-body-<?php echo $key; ?>">
+                                    <span><?php echo $question ?></span>
+                                    <svg data-accordion-icon="" class="w-6 h-6 rotate-180 shrink-0" fill="currentColor"
+                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </h2>
+                            <div id="accordion-flush-body-<?php echo $key; ?>" class=""
+                                aria-labelledby="accordion-flush-heading-<?php echo $key; ?>">
+                                <p class="mt-4 text-gray-500"><?php echo $answer ?></p>
+                                <!-- You can add more content or links here -->
+                            </div>
+                        </div>
+
+                        <?php } ?>
+
+                    </div>
+                </div>
+
+            </div>
+        </section>
+       
+
+
+</section>
+
+
+</div>
+</section>
+
+
+<?php
 
 
 
