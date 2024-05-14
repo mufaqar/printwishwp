b2ccd1085a073e9b83687f1db28c1e482cfe4609
mufaqar <mufaqar@gmail.com>
2024-05-14 13:14:38 +0500

    faqs

--- single-locations.php
+++ single-locations.php
@@ -97,7 +97,7 @@ get_header();
                         <?php endwhile; ?>
 
                     <?php else: ?>
-                        <p>No FAQs found.</p>
+                 
                     <?php endif; ?>
 
                 </div>
