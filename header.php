bc9bd99f224f22cc055913013f3cb43d0e6aaa8c
mufaqar <mufaqar@gmail.com>
2024-06-06 14:38:27 +0500

    updated

--- header.php
+++ header.php
@@ -45,12 +45,10 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-SDRXNDYY1L"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
-
     function gtag() {
         dataLayer.push(arguments);
     }
     gtag('js', new Date());
-
     gtag('config', 'G-SDRXNDYY1L');
     </script>
 
