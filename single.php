d3387ea398b2b75c111c5453063551f0dc4b5290
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2024-06-06 19:51:50 +0500

    Blog

--- single.php
+++ single.php
@@ -10,44 +10,112 @@
 get_header();
 ?>
 
-<section>
+<!-- <section>
     <div class="py-6 relative md:py-16 bg-cover bg-no-repeat bg-center"
         style="background-image: linear-gradient(0deg, rgba(0, 36, 63, 0.7), rgba(0, 36, 63, 0.7)), url('');">
         <div class="container mx-auto px-4 text-white">
             <h2 class="sm:text-4xl text-4xl font-bold font-opensans capitalize mb-2 sm:text-left text-center">
-                <?php the_title()?>
+                <?php the_title() ?>
             </h2>
         </div>
     </div>
-</section>
+</section> -->
 
 
-<main id="primary" class="site-main">
-
-    <div class="container lg:flex  mx-auto px-4">
-
-        <div class="h-20 w-16 text-center pt-6">
-            <span
-                class="text-accent text-3xl font-medium px-2.5 py-0 bg-background block border border-secondary"><?php the_time('j'); ?></span>
-            <span
-                class="bg-secondary text-white font-normal text-sm py-1 leading-normal block box-border px-5"><?php the_time('M'); ?></span>
-        </div>
-        <div class="cont lg:ml-7">
+<main id="primary" class="site-main py-10 bg-gray-100">
+    <div class="container mx-auto px-4 flex md:flex-row flex-col gap-7">
+        <div class="cont md:w-2/3 w-full bg-white p-5">
             <?php
-				while ( have_posts() ) :
-					the_post();
+            while (have_posts()):
+                the_post();
 
-					get_template_part( 'template-parts/content', 'blog' );
-
-					
-					
-
-				endwhile; // End of the loop.
-				?>
-
-			</div>
-			</div>
-
+                get_template_part('template-parts/content', 'blog');
+            endwhile; // End of the loop.
+            ?>
+        </div>
+        <aside class="md:w-1/3 w-full">
+            <div class=''>
+                <h2 class="md:text-xl text-lg font-semibold text-white text-center py-3 bg-secondary">
+                    RELATED PRODUCTS
+                </h2>
+                <div class="bg-white p-3 flex flex-col gap-5">
+                    <div class="flex md:flex-row flex-row gap-3 items-center">
+                        <div class="w-1/3">
+                            <img src="https://printwish.co.uk/wp-content/themes/printwishwp/public//images/6.webp"
+                                alt="feature" class="w-full h-full object-cover object-top" />
+                        </div>
+                        <div>
+                            <a href="#" class="text-sm font-normal text-black hover:text-primary">
+                                Cami Top With Gym Pants For Women
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex md:flex-row flex-row gap-3 items-center">
+                        <div class="w-1/3">
+                            <img src="https://printwish.co.uk/wp-content/themes/printwishwp/public//images/6.webp"
+                                alt="feature" class="w-full h-full object-cover object-top" />
+                        </div>
+                        <div>
+                            <a href="#" class="text-sm font-normal text-black hover:text-primary">
+                                Cami Top With Gym Pants For Women
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex md:flex-row flex-row gap-3 items-center">
+                        <div class="w-1/3">
+                            <img src="https://printwish.co.uk/wp-content/themes/printwishwp/public//images/6.webp"
+                                alt="feature" class="w-full h-full object-cover object-top" />
+                        </div>
+                        <div>
+                            <a href="#" class="text-sm font-normal text-black hover:text-primary">
+                                Cami Top With Gym Pants For Women
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='mt-10'>
+                <h2 class="md:text-xl text-lg font-semibold text-white text-center py-3 bg-secondary">
+                    RECENT POSTS
+                </h2>
+                <div class="bg-white p-3 flex flex-col divide-y divide-black/20">
+                    <div class="flex md:flex-row flex-row gap-3 items-center py-4">
+                        <div class="w-1/3">
+                            <img src="https://printwish.co.uk/wp-content/themes/printwishwp/public//images/6.webp"
+                                alt="feature" class="w-full h-full object-cover object-top" />
+                        </div>
+                        <div>
+                            <a href="#" class="text-sm font-normal text-black hover:text-primary">
+                                Cami Top With Gym Pants For Women
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex md:flex-row flex-row gap-3 items-center py-4">
+                        <div class="w-1/3">
+                            <img src="https://printwish.co.uk/wp-content/themes/printwishwp/public//images/6.webp"
+                                alt="feature" class="w-full h-full object-cover object-top" />
+                        </div>
+                        <div>
+                            <a href="#" class="text-sm font-normal text-black hover:text-primary">
+                                Cami Top With Gym Pants For Women
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex md:flex-row flex-row gap-3 items-center py-4">
+                        <div class="w-1/3">
+                            <img src="https://printwish.co.uk/wp-content/themes/printwishwp/public//images/6.webp"
+                                alt="feature" class="w-full h-full object-cover object-top" />
+                        </div>
+                        <div>
+                            <a href="#" class="text-sm font-normal text-black hover:text-primary">
+                                Cami Top With Gym Pants For Women
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </aside>
+    </div>
 </main><!-- #main -->
 
 <?php
