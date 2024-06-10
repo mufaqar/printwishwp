4c7d9a6a1edcabde2529e8bbe04d1e527a6f7c49
mufaqar <mufaqar@gmail.com>
2024-06-10 14:42:04 +0500

    gmail

--- single-bags.php
+++ single-bags.php
@@ -117,7 +117,7 @@ while (have_posts()) :
                                 email</label>
                             <input type="email" name="email" id="email"
                                 class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base focus:ring-primary focus:border-primary block w-full p-2.5"
-                                placeholder="name@flowbite.com" required />
+                                placeholder="name@gmail.com" required />
                         </div>
                         <div>
                             <label for="quantity"
