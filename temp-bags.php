101c0f5cb4486215c5e98601b03bfe9770822381
mufaqar <mufaqar@gmail.com>
2024-06-10 14:26:02 +0500

    why choose

--- temp-bags.php
+++ temp-bags.php
@@ -9,7 +9,7 @@ get_header();
 
 <section class="container mx-auto px-4">
     <div class="flex justify-center mt-6">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/why-chose.webp" alt="choose" width="810" height="424" />
+        <img src="<?php echo get_template_directory_uri(); ?>/public/images/why-chose.webp" alt="choose" width="810" height="424" />
     </div>
     
     <?php get_template_part( 'components/com', 'brands' ); ?>
