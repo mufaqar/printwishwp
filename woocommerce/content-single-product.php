86810ce251cfdeb504702e0dabde80486f975e7f
mufaqar <mufaqar@gmail.com>
2024-08-09 16:30:04 +0500

    updated layout

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -321,10 +321,43 @@ $faqs = array(
         </section>
 
 
-      
+        <section class='container mx-auto px-3 my-10 '>
+            <h2 class="text-2xl text-center text-primary font-bold leading-4">How To Order?</h2>
+            <div class="bg-[#D9EDF7] w-full mt-4 text-center py-4 font-[700] leading-[1.2em] text-[#070505] ">
+                <p>Send us a quote to get favorite product at the best price. </p>
+            </div>
+            <div class="grid md:grid-cols-4 text-center gap-5 font-[500] grid-cols-2 px-5">
+                <div class="border w-full border-none flex flex-col items-center ">
+                    <Image width="166px" height="166px"
+                        src="<?php bloginfo('template_directory'); ?>/public/images/iocns-01.png" alt="" class="" />
+                    <p class="text-xs md:text-base">Pick your items, select the quantity and sizes.</p>
+                </div>
+                <div class="border w-full border-none flex flex-col items-center">
+                    <Image width="166px" height="166px"
+                        src="<?php bloginfo('template_directory'); ?>/public/images/iocns-02.png" alt="" class="" />
+                    <p class="text-xs md:text-base">Complete the quote form with your details, choose your
+                        print positions, attach your logo, and press submit.</p>
+                </div>
+                <div class="border w-full border-none flex flex-col items-center">
+                    <Image width="166px" height="166px"
+                        src="<?php bloginfo('template_directory'); ?>/public/images/iocns-03.png" alt="" class="" />
+                    <p class="text-xs md:text-base">We'll create a FREE digital proof of your design for your
+                        approval.</p>
+                </div>
+                <div class="border w-full border-none flex flex-col items-center">
+                    <Image width="166px" height="166px"
+                        src="<?php bloginfo('template_directory'); ?>/public/images/iocns-04.png" alt="" class="" />
+                    <p class="text-xs md:text-base">We guarantee to deliver your order by your specified date.
+                    </p>
+                </div>
+            </div>
+        </section>
 
 
-        
+
+
+
+
 
 
 
@@ -365,38 +398,7 @@ $faqs = array(
             <?php  delivery_time();?>
         </div>
 
-        <section class='container mx-auto px-3 my-10 '>
-
-<h2 class="text-2xl text-center text-primary font-bold leading-4">How To Order?</h2>
-<div class="bg-[#D9EDF7] w-full mt-4 text-center py-4 font-[700] leading-[1.2em] text-[#070505] ">
-    <p>Send us a quote to get favorite product at the best price. </p>
-</div>
-<div class="grid md:grid-cols-4 text-center gap-5 font-[500] grid-cols-2 px-5">
-    <div class="border w-full border-none flex flex-col items-center ">
-        <Image width="166px" height="166px"
-            src="<?php bloginfo('template_directory'); ?>/public/images/iocns-01.png" alt="" class="" />
-        <p class="text-xs md:text-base">Pick your items, select the quantity and sizes.</p>
-    </div>
-    <div class="border w-full border-none flex flex-col items-center">
-        <Image width="166px" height="166px"
-            src="<?php bloginfo('template_directory'); ?>/public/images/iocns-02.png" alt="" class="" />
-        <p class="text-xs md:text-base">Complete the quote form with your details, choose your
-            print positions, attach your logo, and press submit.</p>
-    </div>
-    <div class="border w-full border-none flex flex-col items-center">
-        <Image width="166px" height="166px"
-            src="<?php bloginfo('template_directory'); ?>/public/images/iocns-03.png" alt="" class="" />
-        <p class="text-xs md:text-base">We'll create a FREE digital proof of your design for your
-            approval.</p>
-    </div>
-    <div class="border w-full border-none flex flex-col items-center">
-        <Image width="166px" height="166px"
-            src="<?php bloginfo('template_directory'); ?>/public/images/iocns-04.png" alt="" class="" />
-        <p class="text-xs md:text-base">We guarantee to deliver your order by your specified date.
-        </p>
-    </div>
-</div>
-</section>
+       
 
 
 
