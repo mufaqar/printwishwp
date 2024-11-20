ad264b719e6777f1978331c8f2c9b9c5cd649bd3
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-11-20 19:27:28 +0500

    update text

--- header.php
+++ header.php
@@ -64,7 +64,7 @@
 
 
 <button class="">
-<a href="http://localhost/dev1/enquire-now" class="md:text-lg fixed bottom-5 right-5 bg-green-500 py-3 md:px-6 px-5 hover:bg-primary rounded-full text-white" style="z-index: 11111">
+<a href="http://localhost/dev1/enquire-now" class="md:text-lg fixed bottom-5 right-5 bg-green-500 py-2.5 md:py-3 md:px-6 px-4 hover:bg-primary rounded-full text-white" style="z-index: 11111">
     Quick Quote
 </a>
 </button>
